7c11c76366fb70b79c02b07ced2539060d5716a4
JackDaniel67 <palanivelgreensomr@gmail.com>
2023-02-01 10:02:33 +0530

    Jack-Works

--- src/org/base/Test.java
+++ src/org/base/Test.java
@@ -3,8 +3,10 @@ package org.base;
 public class Test {
 	public static void main(String[] args) {
 		
-		String s = " Jack";
+		String s = "Jack";
 		System.out.println(s);
+		boolean contains = s.contains("J");
+		System.out.println(contains);
 		
 		
 	}
